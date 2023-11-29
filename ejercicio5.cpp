7a8e8c46353548c6f5375f8a500af55f926f575d
LuisLinares1205 <00014823@uca.edu.sv>
2023-11-28 18:07:35 -0600

    :añadiendo nuevos cambios al ejercicio 5

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -6,12 +6,15 @@ int main() {
     srand(static_cast<unsigned int>(time(nullptr)));
 
     // Número de experimentos
-    const int num_experimentos = 100;
+     int NumExperimentos;
+
+     std::cout << "Ingrese el numero de expeimrentos a realizar: "<<std::endl;
+     std::cin >> NumExperimentos;
 
     // Contador para contar casos favorables
-    int casos_favorables = 0;
+    int CasosFavorables = 0;
 
-    for (int i = 0; i < num_experimentos; ++i) {
+    for (int i = 0; i < NumExperimentos; ++i) {
         // Lanzamiento de dos dados
         int dado1 = rand() % 6 + 1; // Números entre 1 y 6
         int dado2 = rand() % 6 + 1;
@@ -19,21 +22,21 @@ int main() {
         // Verificar si la cara 6 aparece en al menos uno de los dados
         if (dado1 == 6 || dado2 == 6) {
             // Calcular la suma de los puntos
-            int suma_puntos = dado1 + dado2;
+            int SumaPuntos = dado1 + dado2;
 
             // Verificar si la suma es mayor o igual a 9
-            if (suma_puntos >= 9) {
-                casos_favorables++;
+            if (SumaPuntos >= 9) {
+                CasosFavorables++;
             }
         }
     }
 
     // Calcular la probabilidad
-    double probabilidad = static_cast<double>(casos_favorables) / num_experimentos;
+    double probabilidad = static_cast<double>(CasosFavorables) / NumExperimentos;
 
     // Mostrar resultados
-    std::cout << "Número de experimentos: " << num_experimentos << std::endl;
-    std::cout << "Casos favorables: " << casos_favorables << std::endl;
+    std::cout << "Número de experimentos: " << NumExperimentos << std::endl;
+    std::cout << "Casos favorables: " << CasosFavorables << std::endl;
     std::cout << "Probabilidad P(suma >= 9 | cara 6 aparece): " << probabilidad << std::endl;
 
     return 0;
